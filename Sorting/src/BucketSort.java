921cd364d15efcdea37aa7199980002fbdb7c21c
elasfour <mohamed.elasfoury@epfl.ch>
2019-02-26 17:20:26 +0100

    Ou est Charlie project

---
+++ Sorting/src/BucketSort.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BucketSort {
+
+	public static void main(String[] args) {
+
+		
+		int[] list_numnbers1 = {1,4,5,6,8,9,3,2,0};
+		
+		int[] sortedList1 = new int[9];
+		
+		for(int i =0;i < list_numnbers1.length;i++) {
+			sort]
+		}
+		
+		
+		
+	}
+
+}
